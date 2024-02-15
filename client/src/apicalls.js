9003107e7e6f2d56b2787ffbc50d6425c12d3ca4
NotGyashu <110993626+NotGyashu@users.noreply.github.com>
2024-02-15 16:48:37 +0530

    deploying

--- client/src/apicalls.js
+++ client/src/apicalls.js
@@ -1,12 +1,16 @@
-import axios from "axios"
+import axios from "axios";
 
-export const logincall = async (userCredential,dispatch)=>{
-    dispatch({type:"Login_Start"});
-    try{
-const res = await axios.post("/api/auth/login",userCredential);
+export const logincall = async (userCredential, dispatch) => {
+  const axiosInstance = axios.create({
+    baseURL: String(process.env.REACT_APP_BASE_URL),
+  });
+console.log(axiosInstance.defaults.baseURL);
+  dispatch({ type: "Login_Start" });
+  try {
+    const res = await axiosInstance.post("/api/auth/login", userCredential);
 
-dispatch({type:"Login_Sucess" , payload:res.data});
-    }catch(error){
-dispatch({type:"Login_Fail" , payload:error})
-    }
-}
+    dispatch({ type: "Login_Sucess", payload: res.data });
+  } catch (error) {
+    dispatch({ type: "Login_Fail", payload: error });
+  }
+};
