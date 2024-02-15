10c0df6adf0e216323d7e00a4bde75139da443f2
NotGyashu <110993626+NotGyashu@users.noreply.github.com>
2024-02-16 00:06:20 +0530

    origins

--- API/config/allowedOrigins.js
+++ API/config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["https://social-arena.vercel.app/", "http://localhost:3000/", "*"];
+const allowedOrigins = ["https://social-arena.vercel.app", "http://localhost:3000", "*"];
 
 module.exports = allowedOrigins;
